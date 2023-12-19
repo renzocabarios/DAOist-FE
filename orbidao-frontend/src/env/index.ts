9f84f5549e179890ea1d57ff493ebdfb069f3cde
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-12-19 13:17:11 +0800

    Add env

---
+++ orbidao-frontend/src/env/index.ts
@@ -0,0 +1,11 @@
+export const NEXT_PUBLIC_PROPOSAL_ID: string =
+  process.env.NEXT_PUBLIC_PROPOSAL_ID ?? "";
+
+export const NEXT_PUBLIC_STAKINGL_ID: string =
+  process.env.NEXT_PUBLIC_STAKINGL_ID ?? "";
+
+export const NEXT_PUBLIC_TREASURY_ID: string =
+  process.env.NEXT_PUBLIC_TREASURY_ID ?? "";
+
+export const NEXT_PUBLIC_VOTING_ID: string =
+  process.env.NEXT_PUBLIC_VOTING_ID ?? "";
