e8ee3a1026caa3fc27fe26d29cd64704c8189fbb
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-12-19 13:17:06 +0800

    Barrel idl

---
+++ orbidao-frontend/src/idl/index.ts
@@ -0,0 +1,6 @@
+import { PROPOSAL_IDL } from "@/idl/proposal";
+import { STAKING_IDL } from "@/idl/staking";
+import { TREASURY_IDL } from "@/idl/treasury";
+import { VOTING_IDL } from "@/idl/voting";
+
+export { PROPOSAL_IDL, STAKING_IDL, TREASURY_IDL, VOTING_IDL };
