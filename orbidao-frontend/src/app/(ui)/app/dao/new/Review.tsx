c63440a35120c2263f61fcf7b834a1f4dc5a4a9b
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-01-06 15:11:05 +0800

    Update Review

--- orbidao-frontend/src/app/(ui)/app/dao/new/Review.tsx
+++ orbidao-frontend/src/app/(ui)/app/dao/new/Review.tsx
@@ -1,11 +1,14 @@
-import { Card, CardHeader, CardBody } from '@nextui-org/card';
-import { Typography } from '@/components/ui/Typography';
-import { StepProps } from './page';
-import { MEMBERSHIP } from './Membership';
+import { Card, CardHeader, CardBody } from "@nextui-org/card";
+import { Typography } from "@/components/ui/Typography";
+import { StepProps } from "./page";
+import { MEMBERSHIP } from "./Membership";
 
-const CreateOrgReview = ({ formData }: Pick<StepProps, 'formData'>) => {
+const CreateOrgReview = ({ formData }: Pick<StepProps, "formData">) => {
   const hideAddress = formData.membership === MEMBERSHIP.WHITELIST;
   const isFungible = formData.membership === MEMBERSHIP.FUNGIBLE;
+
+  console.log(formData);
+
   return (
     <div className="flex flex-wrap flex-col gap-2 my-4 items-center text-left">
       <Card className="w-full border border-white bg-gradient-to-b from-zinc-800 to-gray-700">
@@ -36,18 +39,25 @@ const CreateOrgReview = ({ formData }: Pick<StepProps, 'formData'>) => {
           {!hideAddress && (
             <Typography className="flex justify-between">
               <Typography className="pr-10" weight="semibold">
-                {isFungible ? 'Mint Address' : 'Collection Address'}:
+                {isFungible ? "Mint Address" : "Collection Address"}:
               </Typography>
-              <Typography weight="light">{`${formData?.mintAddress || formData?.collectionAddress}`}</Typography>
+              <Typography weight="light">{`${
+                formData?.mintAddress || formData?.collectionAddress
+              }`}</Typography>
             </Typography>
           )}
           <Typography className="flex justify-between">
             <Typography className="pr-10" weight="semibold">
               Proposal Fee:
             </Typography>
-            <Typography weight="light">{`${formData?.proposalFee}`} SOL</Typography>
+            <Typography weight="light">
+              {`${formData?.proposalFee}`} SOL
+            </Typography>
           </Typography>
-          <Typography className="my-2 mt-2 w-full justify-center text-center" as="h5">
+          <Typography
+            className="my-2 mt-2 w-full justify-center text-center"
+            as="h5"
+          >
             Voting Options
           </Typography>
 
@@ -61,27 +71,35 @@ const CreateOrgReview = ({ formData }: Pick<StepProps, 'formData'>) => {
             <Typography className="pr-10" weight="semibold">
               Quorum Participation:
             </Typography>
-            <Typography weight="light">{`${formData?.quorumParticipation}`} %</Typography>
+            <Typography weight="light">
+              {`${formData?.quorumParticipation}`} %
+            </Typography>
           </Typography>
           {!hideAddress && !isFungible && (
             <Typography className="flex justify-between">
               <Typography className="pr-10" weight="semibold">
                 Stake:
               </Typography>
-              <Typography weight="light">{`${formData?.votingStake}`} tokens</Typography>
+              <Typography weight="light">
+                {`${formData?.votingStake}`} tokens
+              </Typography>
             </Typography>
           )}
           <Typography className="flex justify-between">
             <Typography className="pr-10" weight="semibold">
               Pre-voting Duration:
             </Typography>
-            <Typography weight="light">{`${formData?.preVoting}`} Days</Typography>
+            <Typography weight="light">
+              {`${formData?.preVoting}`} Days
+            </Typography>
           </Typography>
           <Typography className="flex justify-between">
             <Typography className="pr-10" weight="semibold">
               Post-voting Duration:
             </Typography>
-            <Typography weight="light">{`${formData?.postVoting}`} Days</Typography>
+            <Typography weight="light">
+              {`${formData?.postVoting}`} Days
+            </Typography>
           </Typography>
         </CardBody>
       </Card>
