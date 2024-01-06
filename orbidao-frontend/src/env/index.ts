1655e5a53137cd9d088e8409056c4e9cbefd4492
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-01-06 15:10:36 +0800

    Add NEXT_PUBLIC_ISSUE_ID

--- orbidao-frontend/src/env/index.ts
+++ orbidao-frontend/src/env/index.ts
@@ -1,11 +1,14 @@
 export const NEXT_PUBLIC_PROPOSAL_ID: string =
   process.env.NEXT_PUBLIC_PROPOSAL_ID ?? "";
 
-export const NEXT_PUBLIC_STAKINGL_ID: string =
-  process.env.NEXT_PUBLIC_STAKINGL_ID ?? "";
+export const NEXT_PUBLIC_STAKING_ID: string =
+  process.env.NEXT_PUBLIC_STAKING_ID ?? "";
 
 export const NEXT_PUBLIC_DAO_2_ID: string =
   process.env.NEXT_PUBLIC_DAO_2_ID ?? "";
 
 export const NEXT_PUBLIC_VOTING_ID: string =
   process.env.NEXT_PUBLIC_VOTING_ID ?? "";
+
+export const NEXT_PUBLIC_ISSUE_ID: string =
+  process.env.NEXT_PUBLIC_ISSUE_ID ?? "";
