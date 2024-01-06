6603ae345c725778a76f4c6ca0c75fd9eaa1e28e
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-01-06 15:09:47 +0800

    Add getProposalConfigKey

--- orbidao-frontend/src/programs/proposal.ts
+++ orbidao-frontend/src/programs/proposal.ts
@@ -3,6 +3,13 @@ import { AnchorProvider, BN } from "@coral-xyz/anchor";
 import { getProposalProgram } from "@/utils";
 import { NEXT_PUBLIC_PROPOSAL_ID } from "@/env";
 
+export function getProposalConfigKey(seed: BN) {
+  return PublicKey.findProgramAddressSync(
+    [Buffer.from("config"), seed.toArrayLike(Buffer, "le", 8)],
+    new PublicKey(NEXT_PUBLIC_PROPOSAL_ID)
+  )[0];
+}
+
 export async function initializeProposalProgram(
   provider: AnchorProvider,
   seed: BN,
