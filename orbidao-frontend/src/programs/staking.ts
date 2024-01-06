77798438b94c5cc86b1569a99867620715c0f6eb
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-01-06 15:09:40 +0800

    Add getStakingConfigKey

--- orbidao-frontend/src/programs/staking.ts
+++ orbidao-frontend/src/programs/staking.ts
@@ -1,13 +1,20 @@
 import { PublicKey } from "@solana/web3.js";
 import { AnchorProvider, BN } from "@coral-xyz/anchor";
 import { getStakingProgram } from "@/utils";
-import { NEXT_PUBLIC_STAKINGL_ID } from "@/env";
+import { NEXT_PUBLIC_STAKING_ID } from "@/env";
+
+export function getStakingConfigKey(seed: BN) {
+  return PublicKey.findProgramAddressSync(
+    [Buffer.from("config"), seed.toArrayLike(Buffer, "le", 8)],
+    new PublicKey(NEXT_PUBLIC_STAKING_ID)
+  )[0];
+}
 
 export async function initializeStakingProgram(
   provider: AnchorProvider,
   seed: BN,
   issuePublickey: PublicKey
 ) {
-  const program = getStakingProgram(NEXT_PUBLIC_STAKINGL_ID, provider);
+  const program = getStakingProgram(NEXT_PUBLIC_STAKING_ID, provider);
   return await program.methods.initialize(seed, issuePublickey).accounts({});
 }
