4d85003921e5dad050507b6e7fd3959224a8a08b
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-01-20 12:16:39 +0800

    Update env

--- umbrella_dao/env/index.ts
+++ umbrella_dao/env/index.ts
@@ -1,5 +1,21 @@
 import { Cluster, clusterApiUrl } from "@solana/web3.js";
 
+export const NEXT_PUBLIC_PROPOSAL_ID: string =
+  process.env.NEXT_PUBLIC_PROPOSAL_ID ??
+  "propm845StqEBV57ZSnTe8EW8duzAxo5p7h4inhibXV";
+
+export const NEXT_PUBLIC_STAKING_ID: string =
+  process.env.NEXT_PUBLIC_STAKING_ID ??
+  "stakyTBmEpbUcxNhjiv16Bvr53RVy68ENBZXPiUzNcF";
+
+export const NEXT_PUBLIC_DAO_2_ID: string =
+  process.env.NEXT_PUBLIC_DAO_2_ID ??
+  "daoSYkGVA6pu5CxknvVMMTc8nFAGsYzfQt2jK5CgC5V";
+
+export const NEXT_PUBLIC_VOTING_ID: string =
+  process.env.NEXT_PUBLIC_VOTING_ID ??
+  "voteUXym9t6h3VzHYumMyMCXmiDoYqMiC2g4JnvdnGC";
+
 export const NEXT_PUBLIC_CLUSTER: Cluster = (process.env.NEXT_PUBLIC_CLUSTER ??
   "devnet") as Cluster;
 
