2af3fbec403b1a17034c31da499c9f4f3e511329
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-01-20 11:59:00 +0800

    umbrella_dao

---
+++ umbrella_dao/app/page.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <div className="flex justify-between items-center">
+        <p>
+          Build in our DAO a sense of Oneness A feeling of Strength to be
+          derived from Unity
+        </p>
+        <div className="flex justify-between items-center gap-4">
+          <p>About Us</p>
+          <p>Connect Wallet</p>
+        </div>
+      </div>
+
+      <div className="flex justify-center items-center">
+        <p>LOGO</p>
+      </div>
+
+      <div className="flex justify-evenly items-center gap-4">
+        <p>Members</p>
+        <p>New Users</p>
+      </div>
+      <div className="flex justify-center items-center">
+        <p>
+          Unleashing the Freedom of Decentralized Autonomous Organizations
+          through Utility NFTs across Solana
+        </p>
+      </div>
+
+      <div className="flex justify-evenly items-center">
+        <p>Twitter</p>
+        <p>Medium</p>
+        <p>Discord</p>
+        <p>Telegram</p>
+        <p>#3Thos by WBA</p>
+      </div>
+    </main>
+  );
+}
