810c54e21be15fa0bb83ed42a680f696b05afd3b
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-02-13 15:56:27 +0800

    Update Home

--- umbrella_dao/app/page.tsx
+++ umbrella_dao/app/page.tsx
@@ -2,10 +2,18 @@
 import { MainLayout } from "@/layouts";
 require("@solana/wallet-adapter-react-ui/styles.css");
 
+import { DaoCard } from "@/components";
 export default function Home() {
   return (
     <MainLayout>
-      <p>Hello World</p>
+      <div className="flex flex-col gap-6">
+        <p className="text-lg font-bold text-white">Featured DAOs</p>
+        <div className="flex items-center gap-4">
+          <DaoCard />
+          <DaoCard />
+          <DaoCard />
+        </div>
+      </div>
     </MainLayout>
   );
 }
