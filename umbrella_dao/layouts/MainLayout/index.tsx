5134e5673f5e2deb25bcc6c97bc268a893905e6e
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-02-13 16:28:13 +0800

    Update MainLayout

--- umbrella_dao/layouts/MainLayout/index.tsx
+++ umbrella_dao/layouts/MainLayout/index.tsx
@@ -1,12 +1,18 @@
+"use client";
 import React from "react";
 import style from "./style.module.css";
-
+import { useRouter } from "next/navigation";
 function MainLayout({ children }: any) {
+  const router = useRouter();
   return (
     <main>
       <div className="z-10">
         <div className={style.navbar}></div>
-        <div className={style.sidenav}></div>
+        <div className={style.sidenav}>
+          <p className="text-gray-100" onClick={() => router.push("/create")}>
+            Create
+          </p>
+        </div>
       </div>
       <div className={style.container}>
         <div className={style.sidenav_divider}></div>
