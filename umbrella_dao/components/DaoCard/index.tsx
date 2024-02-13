3acf67fda316c807bb1054ba62da818835ef3976
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-02-13 15:56:16 +0800

    Add DaoCard

---
+++ umbrella_dao/components/DaoCard/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Image from "next/image";
+import image from "@/public/image.png";
+interface IDaoCard {}
+
+function DaoCard({}: IDaoCard) {
+  return (
+    <div className="bg-slate-700 p-4 w-60 rounded-lg flex flex-col gap-6 text-gray-500">
+      <div className="flex flex-col items-center gap-2">
+        <Image src={image} width={100} height={100} alt="profile-pic" />
+        <div className="flex flex-col items-center">
+          <p className="font-semibold text-gray-100">DAO DAO</p>
+          <p className="text-sm">August 2023</p>
+        </div>
+      </div>
+
+      <div className="flex flex-col w-full gap-2">
+        <p className=" text-gray-200 text-sm">A DAO that builds DAOs</p>
+        <div className="flex flex-col">
+          <p className="text-xs">$9.26k est. USD value</p>
+          <p className="text-xs">14 proposals</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DaoCard;
--- umbrella_dao/components/index.ts
+++ umbrella_dao/components/index.ts
@@ -1,3 +1,4 @@
 import InputField from "./InputField";
 import Button from "./Button";
-export { Button, InputField };
+import DaoCard from "./DaoCard";
+export { DaoCard, Button, InputField };
